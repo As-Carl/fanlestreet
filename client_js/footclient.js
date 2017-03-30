0a9c197911df987f2fb3c97b423f6b86210bf878
zzb <694344795@qq.com>
2017-03-30 19:14:57 +0800

    zzb.3.30.2

--- client_js/footclient.js
+++ client_js/footclient.js
@@ -1,10 +1,34 @@
-var footapp = angular.module('footapp',['commonApp']);
-footapp.controller('footcontroller',['$scope','$http',function($scope,$http){
-	$http({
-		url: common.baseurl + 'foot',
-		method:'get',
-	}).success(function(res){
-		console.log(res);
-		$scope.data = res;
+var footapp = angular.module('footapp', ['commonApp']);
+footapp.controller('footcontroller', ['$scope', '$http', function($scope, $http) {
+	
+			$http({
+				url: common.baseurl + 'foot',
+				method: 'post',
+				data: {page: 5}
+			}).success(function(res) {
+				console.log(res);
+				$scope.data = res;
+
+			})
+		var i = 1;
+		$(window).scroll(function() {
+		console.log(scrollY);
+		console.log($('.banner').clientTop)
+		console.log($('.banner').offsetY);
+		var y = parseInt(scrollY /150)
+		if(y == 1*i) {
+			console.log(i);
+			i++;
+			$http({
+				url: common.baseurl + 'foot',
+				method: 'post',
+				data: {page: i*5}
+			}).success(function(res) {
+				console.log(res);
+				$scope.data = res;
+
+			})
+		}
 	})
+
 }])
